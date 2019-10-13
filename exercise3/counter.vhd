d6bf8a1d3b702f4abfb0e77b3dea7d3be954c81b
OCCE <oscarbjorkgren@gmail.com>
2019-10-13 11:58:01 +0300

    completed exercise 3

--- exercise3/counter.vhd
+++ exercise3/counter.vhd
@@ -1,58 +1,105 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 09.10.2019 23:58:12
--- Design Name: 
--- Module Name: counter - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity counter is
-    Port ( enable : in STD_LOGIC;
+    generic(vector_size: integer:=5);
+    Port ( 
+           clock : in std_ulogic;
+           enable : in STD_LOGIC;
            reset : in STD_LOGIC;
            downup : in STD_LOGIC;
            load : in STD_LOGIC;
-           data : in std_logic_vector(4 downto 0);
-           count : out std_logic_vector(4 downto 0);
+           data : in std_logic_vector(vector_size-1 downto 0);
+           count : out std_logic_vector(vector_size-1 downto 0);
            over : out STD_LOGIC);
 end counter;
 
+-- Counter Architecture 1 - Asynchronous
 architecture counter_model1 of counter is
-
 begin
-
-
+    -- All inputs are in sensitivity list -> async
+    process(clock,enable,reset,downup,load,data)
+    
+    variable count_int:integer:=0;
+    variable over_count:std_logic:='0';
+    
+    begin
+        -- Reset async
+        if(reset = '1') then
+            count_int:=0;
+            count<=std_logic_vector(TO_UNSIGNED(count_int,vector_size));
+            over_count:='0';
+            over<=over_count;  
+        end if;
+        -- Load intermediate output values according to input
+        if(load='1') then
+            count_int:=to_integer(signed(data));
+            over_count:='0'; 
+        elsif(downup = '1' AND rising_edge(clock) AND enable = '1') then
+            count_int:=count_int+1;
+            if(count_int>15) then 
+                count_int:=0;
+                over_count:='1';
+            end if;
+        elsif(downup = '0'  AND rising_edge(clock) AND enable = '1') then
+            count_int:=count_int-1;
+            if(count_int<0) then 
+                count_int:=15;
+                over_count:='1';
+            end if;
+        end if;
+        -- Assign output at rising edge of the clock
+        if(rising_edge(clock) AND enable = '1') then
+            count<=std_logic_vector(TO_UNSIGNED(count_int,vector_size));
+            over<=over_count;
+        end if;
+    end process;
+    
 end counter_model1;
 
+-- Counter Architecture 2 - Synchronous
 architecture counter_model2 of counter is
-
-begin -- include only signals that are read, triggers uppdate
-    process (enable, reset, load)
+begin
+    -- React only to clock and reset
+    process (clock, reset) 
+    
+    variable count_int:integer:=0;
+    variable over_count:std_logic:='0';
+    
     begin
+        -- Reset async
+        if(reset = '1') then
+            count_int:=0;
+            count<=std_logic_vector(TO_UNSIGNED(count_int,vector_size));
+            over_count:='0';
+            over<=over_count;  
+        end if;
+        -- Assign intermediate output values on rising edge
+        if(rising_edge(clock) AND enable = '1') then
+            if(load='1') then
+                count_int:=to_integer(signed(data));
+                over_count:='0';    
+            else 
+                if(downup = '1') then
+                    count_int:=count_int+1;
+                elsif(downup = '0') then
+                    count_int:=count_int-1;
+                end if;
+                if(count_int>15) then 
+                    count_int:=0;
+                    over_count:='1';
+                elsif(count_int<0) then 
+                    count_int:=15;
+                    over_count:='1';
+                end if;
+            end if;
+            -- Assign output     
+            count<=std_logic_vector(TO_UNSIGNED(count_int,vector_size));
+            over<=over_count;  
+        end if;
+
     
     end process;
+    
 end counter_model2;
--- exercise3/counter_test.vhd
+++ exercise3/counter_test.vhd
@@ -1,4 +1,3 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -6,29 +5,54 @@ entity counter_test is
 end counter_test;
 
 architecture Behavioral of counter_test is
-    component counter
-        port ( enable : in STD_LOGIC;
-           reset : in STD_LOGIC;
-           downup : in STD_LOGIC;
-           load : in STD_LOGIC;
-           data : in std_logic_vector(4 downto 0);
-           count : out std_logic_vector(4 downto 0);
-           over : out STD_LOGIC);
-    end component;
-    
+
+    -- Common Inputs
+    signal clock_test : std_ulogic:='0';
     signal enable_test : STD_LOGIC;
-    signal reset_test : STD_LOGIC;
+    signal reset_test : STD_LOGIC:='1';
     signal downup_test : STD_LOGIC;
     signal load_test : STD_LOGIC;
     signal data_test : std_logic_vector(4 downto 0);
-    signal count_test : std_logic_vector(4 downto 0);
-    signal over_test : STD_LOGIC;
+    
+    -- Separate Outputs
+    signal count_test_m1 : std_logic_vector(4 downto 0);
+    signal over_test_m1 : STD_LOGIC;
+    signal count_test_m2 : std_logic_vector(4 downto 0);
+    signal over_test_m2 : STD_LOGIC;
 
 begin
-    DUT: counter port map (enable => enable_test, reset => reset_test, downup => downup_test, load => load_test, data => data_test, count => count_test, over => over_test);
+    -- Instantiate entities for the two architectures, map ports
+    -- work. compares to this. in Java
+    M1: entity work.counter(counter_model1)
+    port map (clock => clock_test, enable => enable_test, reset => reset_test, downup => downup_test, load => load_test, data => data_test, count => count_test_m1, over => over_test_m1);
+    M2: entity work.counter(counter_model2)
+    port map (clock => clock_test, enable => enable_test, reset => reset_test, downup => downup_test, load => load_test, data => data_test, count => count_test_m2, over => over_test_m2);
     
+    -- Generate clock signal
+    clock_test<= not clock_test after 10ns;
+    
+    -- Loadable data
+    data_test<="00011";
+
+    -- Process containing main functionality of the counter
     process
     begin
+        downup_test<='1';
+        wait for 20ns;
+        reset_test<='0';
+        wait for 50ns;
+        enable_test<='1';
+        wait for 200ns;
+        load_test<='1';
+        wait for 20ns;
+        load_test<='0';
+        wait for 200ns;
+        downup_test<='0';
+        wait for 400ns;
+        reset_test<='1';
+        wait for 20ns;
+        reset_test<='0'; 
     end process;
+
 
 end Behavioral;
