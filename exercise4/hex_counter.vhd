47366fd5953c3390bb38b07c4717463cc34d94e9
OCCE <oscarbjorkgren@gmail.com>
2019-10-21 20:39:56 +0300

    Exercise 4 completed

--- exercise4/hex_counter.vhd
+++ exercise4/hex_counter.vhd
@@ -1,14 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity hex_counter is
     generic(
@@ -23,151 +15,79 @@ entity hex_counter is
            data : in std_logic_vector((digit_length*2)-1 downto 0);
            digit1 : out std_logic_vector(digit_length-1 downto 0);
            digit2 : out std_logic_vector(digit_length-1 downto 0);
-           over: out STD_LOGIC
+           over: out std_logic
            );
 end hex_counter;
 
 architecture Behavioral of hex_counter is
 
-signal count_lock: boolean:=True;   
-signal count_hex: integer:=0;
+signal count_next: integer:=16#00#;
+signal count_curr: integer:=16#00#;
 signal over_count: std_logic:='0';
+
 begin
 
---    -- Combinatorial (logic) process
-    process(clk)
-
+    -- Combinatorial (logic) process
+    process(enable,load,data,downup,count_curr)
+    variable bias: integer:=1;
     begin
-        if(load='1') then
-            count_hex<=to_integer(unsigned(data));
-        end if;
+        if(enable='1') then
         
-        if(rising_edge(clk) AND load='0' and NOT count_lock) then
+            -- Determine count direction
             if(downup='0') then
-               count_hex<=count_hex-1;
+               bias:=-1;
             elsif(downup='1') then
-               count_hex<=count_hex+1;
+               bias:=1;
             end if;
+                        
+            -- Load data 
+            if(load='1') then
+               count_next<=to_integer(unsigned(data));
+               over_count<='0';
+               
+            -- Add/subtract when current count is updated with next count
+            elsif(count_curr=count_next) then
                 
-            if(count_hex>16#FF#) then
-               count_hex<=16#00#;
-            elsif(count_hex<16#00#) then
-               count_hex<=16#FF#;
-            end if;           
-        end if;
+                count_next<=count_next+bias;
+                -- Handle overflow
+                if(count_next+bias>16#FF#) then
+                    over_count<='1';
+                    count_next<=16#00#;
+                -- Handle underflow    
+                elsif(count_next+bias<16#00#) then
+                    over_count<='1';
+                    count_next<=16#FF#;
+                else
+                    over_count<='0';
+                end if;
+     
+            end if;
+        end if;            
     end process;
 
     -- Register (clock) process
     process(clk, rst)
+    variable count_vector: std_logic_vector(7 downto 0);
     begin
-        
+    
+        -- Async reset
         if(rst = '1') then
             digit1<= std_logic_vector(TO_UNSIGNED(16#0#,digit_length)); -- reset to 0
             digit2<= std_logic_vector(TO_UNSIGNED(16#0#,digit_length)); -- reset to 0
-            over_count<='0';
-            over<=over_count;  
-        end if;
-        
-        if(rising_edge(clk)) then
-            count_lock<=True;
+            over<='0';    
             
-            digit1<= std_logic_vector(TO_UNSIGNED(count_hex mod 16#F#,digit_length));
-            digit2<= std_logic_vector(TO_UNSIGNED(count_hex rem 16#F#,digit_length));   
+        -- Catch rising edge and assign output       
+        elsif(rising_edge(clk)) then
+            count_vector:=std_logic_vector(TO_UNSIGNED(count_next,digit_length*2));
+            
+            digit1<= count_vector(7 downto 4);
+            digit2<= count_vector(3 downto 0);
+               
             over<=over_count;
-             
-            if(over_count='1') then
-               over_count<='0';
-            end if;
-                          
-            count_lock<=False;       
+            
+            -- Update current count with next -> allow logic process to add/subtract 
+            count_curr<=count_next;
         end if;
     end process;
-    
-    
-    
-    
---    process (clk, rst) 
---    variable data_int:integer:=0;
---    variable d1:integer:=0;
---    variable d2:integer:=0;
---    variable bias:integer:=1;
---    variable over_count:std_logic:='0';
-    
---    begin
---        -- Reset async
---        if(rst = '1') then
---            digit1<= std_logic_vector(TO_UNSIGNED(16#0#,digit_length)); -- reset to 0
---            digit2<= std_logic_vector(TO_UNSIGNED(16#0#,digit_length)); -- reset to 0
---            over_count:='0';
---            over<=over_count;  
---        end if;
---        -- Assign intermediate output values on rising edge
---        if(rising_edge(clk) AND enable = '1') then
---            if(load='1') then
---                data_int:=to_integer(unsigned(data));
---                if(data_int>16#F#) then
---                    d2:=data_int-16#F#;                    
---                    d1:=16#F#;
---                else 
---                    d1:=data_int;
---                    d2:=16#0#;
---                end if;
---                over_count:='0';    
---            else 
---                -- Count up
---                if(downup = '1') then
---                    bias:=1;
---                    -- Count on D2 when D1 is F
---                    if(d1=16#F#) then
---                        d1:=16#F#;
---                        d2:=d2+bias;
---                    end if;      
---                    -- Count on D1 when D2 is 0
---                    if(d2=16#0#) then
---                        d1:=d1+bias;
---                        d2:=16#0#;
---                    end if;
-                    
---                -- Count down                                     
---                elsif(downup = '0') then
---                    bias:=-1;
---                    -- Count on D1 when D2 is 0
---                    if(d2=16#0#) then
---                        d1:=d1+bias;
---                        d2:=16#0#;
---                    end if;                       
---                    -- Count on D2 when D1 is F
---                    if(d1=16#F#) then
---                        d1:=16#F#;
---                        d2:=d2+bias;
---                    end if;
---                end if;
-                
---                -- Check overflow
---                -- When D2 is over F reset values, aka when count is > 30 -> 0
---                if(d2>16#F#) then 
---                    d1:=16#0#;
---                    d2:=16#0#;
---                    over_count:='1';
---                end if;
---                -- When D1 is less than 0 reset values, aka when count is < 0 -> 30
---                if(d1<16#0#) then 
---                    d2:=16#F#;
---                    d1:=16#F#;
---                    over_count:='1'; 
---                end if;
-                
---            end if;
-          
---            digit1<=std_logic_vector(TO_UNSIGNED(d1,digit_length));
---            digit2<=std_logic_vector(TO_UNSIGNED(d2,digit_length));
---            over<=over_count; 
-             
---            if(over_count='1') then
---                over_count:='0';
---            end if;
-            
---        end if;
---    end process;
     
 end Behavioral;
--- exercise4/hex_counter_test.vhd
+++ exercise4/hex_counter_test.vhd
@@ -31,7 +31,7 @@ architecture Behavioral of hex_counter_test is
            data : in std_logic_vector;
            digit1 : out std_logic_vector;
            digit2 : out std_logic_vector;
-           over: out STD_LOGIC
+           over: out std_logic
            );
     end component;
     
@@ -45,38 +45,34 @@ architecture Behavioral of hex_counter_test is
     -- Outputs
     signal digit1_test : std_logic_vector(d_length-1 downto 0);
     signal digit2_test : std_logic_vector(d_length-1 downto 0);
-    signal over_test : STD_LOGIC;
+    signal over_test : std_logic;
     
 begin
     DUT: hex_counter
     generic map(digit_length => d_length)
     port map(clk => clock_test, enable => enable_test, rst => reset_test, downup => downup_test, load => load_test, data => data_test, digit1 => digit1_test, digit2 => digit2_test, over => over_test);
     
+    -- Clear reset    
     reset_test<='0' after 10ns;
-    
+
     -- Generate clock signal
     clock_test <= not clock_test after 10ns;
     
     -- Loadable data
---    data_test<="00011011";
+    data_test<="00011011";
     
---    process
---    begin
---        downup_test<='1';
---        wait for 20ns;
---        reset_test<='0';
---        wait for 50ns;
---        enable_test<='1';
---        wait for 200ns;
---        load_test<='1';
---        wait for 20ns;
---        load_test<='0';
---        wait for 200ns;
---        downup_test<='0';
---        wait for 400ns;
---        reset_test<='1';
---        wait for 20ns;
---        reset_test<='0'; 
---    end process;
+    process
+    begin
+        wait for 2000ns;
+        load_test<='1';
+        wait for 20ns;
+        load_test<='0';
+        wait for 1000ns;
+        downup_test<='0';
+        wait for 1000ns;
+        enable_test<='0';
+        wait for 100ns;
+        enable_test<='1';
+    end process;
 
 end Behavioral;
